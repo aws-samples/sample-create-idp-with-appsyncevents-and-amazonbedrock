b01df522e801eed470be2ee27343c94ba5f7032e
mamrane <mamrane@amazon.com>
2025-06-18 19:07:50 -0700

    Fix region for amplify frontend configuration

--- amplify/create-amplify-project.sh
+++ amplify/create-amplify-project.sh
@@ -1,3 +1,17 @@
+# if region not provided as an argument, exit
+region = $1
+if [ -z "$region" ]; then
+    # Attempt to set the region
+    region = $(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
+fi
+
+# if region not provided as an argument, exit
+if [ -z "$region" ]; then
+    echo "Please provide the AWS region as an argument."
+    exit 1
+fi
+
+aws_region=$1
 AMPLIFY_FRONTEND_CODE="frontend-code"
 AMPLIFY_PROJECT="amplify-idp"
 
@@ -5,7 +19,7 @@ AMPLIFY_PROJECT="amplify-idp"
 npm create vite@latest $AMPLIFY_PROJECT --yes -- --template react-ts
 
 # Create configuration file
-(cd $AMPLIFY_FRONTEND_CODE; ./create_custom_configuration.sh)
+(cd $AMPLIFY_FRONTEND_CODE; ./create_custom_configuration.sh $region)
 
 # Copy frontend code in Amplify project
 cp -r $AMPLIFY_FRONTEND_CODE/* $AMPLIFY_PROJECT/;

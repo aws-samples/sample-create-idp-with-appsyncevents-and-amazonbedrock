9c534e8f873680d019274e027e6fe434f36b3dab
mamrane <mamrane@amazon.com>
2025-06-18 20:44:10 -0700

    Add required region in create-webapp.sh

--- amplify/create-webapp.sh
+++ amplify/create-webapp.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# if region not provided as an argument, attempt to set the region
+region="$1"
+if [ -z "$region" ]; then
+    region=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
+fi
+
+# if not able to set the region, exit
+if [ -z "$region" ]; then
+    echo "Please provide the AWS region as an argument."
+    exit 1
+fi
+
 # Script to run Amplify setup scripts sequentially
 echo "Starting Amplify setup process..."
 
@@ -16,7 +28,7 @@ fi
 
 # Run the first script
 echo "Running create-amplify-project.sh..."
-./create-amplify-project.sh
+./create-amplify-project.sh "$region"
 if [ $? -ne 0 ]; then
     echo "Error: create-amplify-project.sh failed"
     exit 1
